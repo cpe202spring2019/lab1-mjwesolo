7aa67ea804d98f37f09428527f4a6c5c893978b0
mjwesolo <mjwesolo@unix1.csc.calpoly.edu>
2019-04-08 10:21:47 -0700

    edit #1

--- lab1.py
+++ lab1.py
@@ -1,15 +1,26 @@
 
+
+
+"""finds the max of a list of numbers and returns the value(not the index)
+If int_list is empty, it returns None. If list is None, raises ValueError"""
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    maximum = 100000
+    if len(int_list) == 0:     
+        return None
+    for item in int_list:
+        if item > maximum:
+            maximum = item
+    return maximum
 
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
+ """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+def reverse_rec(int_list):   # must use recursion
+    if 
 
-def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
+
+ """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+def bin_search(target, low, high, int_list):  # must use recursion
+    for i in range(low, high+1):
+    
+    pass
